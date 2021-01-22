bb09a72bd1a81417e88bff4e6b511d86c683b6c5
ggahui <dbrkgml97@nate.com>
2021-01-22 11:14:04 +0900

    [Modify] Message timestamp 추가

--- auth_server/src/main/java/com/gaga/auth_server/dto/message/Message.java
+++ auth_server/src/main/java/com/gaga/auth_server/dto/message/Message.java
@@ -3,27 +3,41 @@ package com.gaga.auth_server.dto.message;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Setter
 @Getter
 public class Message {
-    private String message = "";
-    private Object data = "";
+    String timestamp;
+    private String message;
+    private Object data;
 
     private static final String DEFAULT_KEY = "result";
 
-    public Message() {}
+    public Message() {
+        this.timestamp = convertDateFormat(LocalDateTime.now());
+    }
 
     public Message(String message) {
+        this.timestamp = convertDateFormat(LocalDateTime.now());
         this.message = message;
     }
 
     public Message(Object result) {
+        this.timestamp = convertDateFormat(LocalDateTime.now());
+        this.message = "success";
         this.data = result;
-        this.message = "성공";
     }
 
     public Message(Object result, String message) {
-        this.data = result;
+        this.timestamp = convertDateFormat(LocalDateTime.now());
         this.message = message;
+        this.data = result;
+    }
+
+    private String convertDateFormat(LocalDateTime now) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return now.format(formatter);
     }
 }
